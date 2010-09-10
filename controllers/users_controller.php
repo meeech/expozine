f4a0e66fec1c7f692a006ca8be76988ce05491b1
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-09-09 22:31:55 -0400

    turn on auth

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -3,5 +3,28 @@ class UsersController extends AppController {
 
 	var $name = 'Users';
     var $scaffold;
+    
+    var $components = array('Auth');
+
+    function beforeFilter() {
+        $this->Auth->fields = array(
+            'username' => 'email',
+            'password' => 'password'
+        );
+       
+    }
+
+    /**
+     *  The AuthComponent provides the needed functionality
+     *  for login, so you can leave this function blank.
+     */
+    function login() {
+
+    }
+
+    function logout() {
+        $this->redirect($this->Auth->logout());
+    }
+
 
 }
