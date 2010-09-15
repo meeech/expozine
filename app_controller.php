9ae5f532b96f8e25058a8379d5ce48d9178a4573
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-09-15 00:11:43 -0400

    add some lang related stuff

--- app_controller.php
+++ app_controller.php
@@ -32,14 +32,71 @@
  */
 class AppController extends Controller {
 
-    var $components = array('Auth','Session');
+    var $components = array('Auth','Session', 'DebugKit.Toolbar');
+
+
+	/**
+	 * Available languages
+	 * key is used for locale, value for the global $lang/urls 
+	 * If no language is specified, defaults to first language in array.
+	 *
+	 * @var array
+	 */
+    public $languages = array(
+        'eng'=>'en',
+        'fre'=>'fr', 
+        // 'ger'=>'de', 
+        // 'spa'=>'es'
+    );
+
+    /**
+     * Set in the beforeFilter.                                                     
+     * Gives all controllers access to a  convenient spot for the request language. 
+     *
+     * @var string
+     */    
+    public $requestLanguage = null;
+
 
     function beforeFilter() {
         $this->Auth->fields = array(
             'username' => 'email',
             'password' => 'password'
         );
+        
+        $this->initLanguage();
        
     }
+
+	/**
+	 * Just setup variety of lang related stuff. requestLanguage, Config.language
+	 * We can also call this from the app_error functions to init lang / locale
+	 *
+	 * @return void
+	 **/
+	function initLanguage() {
+        //Set default
+        $reqLang = reset($this->languages);
+
+        //Check if one came through, and its on the list.
+        if( isset($this->params['language']) && in_array($this->params['language'], $this->languages) ) {
+            $reqLang = $this->params['language']; 
+        }
+
+        //And figure out the locale lang. 
+        //Use $this->languages array key for the current lang
+        $localeLang = array_search($reqLang, $this->languages);
+
+        $this->requestLanguage = $reqLang;
+        Configure::write('Config.language', $localeLang);
+        
+        //We need this accessible for generating the 'alternate' links/ lang switcher around the site
+        $this->set('site_languages', $this->languages);
+        $this->set('lang', $reqLang);
+	}
+
+
+
+
 
 }
