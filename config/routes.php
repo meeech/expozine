5de2163330ac8688c696e49d375895c0f140170a
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-10-04 10:09:30 -0400

    fix route to do controller/action

--- config/routes.php
+++ config/routes.php
@@ -27,6 +27,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/views/pages/home.ctp)...
  */
+Router::connect('/:language/:controller/:action');
 Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
 Router::connect('/:language', array('controller' => 'pages', 'action' => 'display', 'home'));
 Router::connect('/:language/*', array('controller' => 'pages', 'action' => 'display'), array('language' => '[a-z]{2}'));
